e91605ee6986985999b3245dafc532c8f53a7832
Falgor C <tranhoa.7604@gmail.com>
2021-05-10 08:45:05 +0700

    Initial Commit

---
+++ sr_ios_storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sr_ios_storyboard
+//
+//  Created by Hoa Tran on 5/10/21.
+//  Copyright © 2021 Falgor C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ sr_ios_storyboardTests/sr_ios_storyboardTests.swift
@@ -0,0 +1,34 @@
+//
+//  sr_ios_storyboardTests.swift
+//  sr_ios_storyboardTests
+//
+//  Created by Hoa Tran on 5/10/21.
+//  Copyright © 2021 Falgor C. All rights reserved.
+//
+
+import XCTest
+@testable import sr_ios_storyboard
+
+class sr_ios_storyboardTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
